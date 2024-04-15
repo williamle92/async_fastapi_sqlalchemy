14d4320c0ad5d0c635ca1cb21d8dacf34534d5bc
William Le <williamkle92@gmail.com>
2024-04-14 23:00:54 -0700

    feat: auth pydantic objects

--- backend/pydantic/auth.py
+++ backend/pydantic/auth.py
@@ -1,37 +1,37 @@
 from fastapi import Form
 from pydantic import BaseModel, EmailStr, Field
-from datetime import datetime
 
 
 class Token(BaseModel):
-	token: str
-	expiration: datetime
+    access_token: str
+    expiration: str
+    type: str = Field("bearer")
 
 
 class OAuthRequestForm:
-	"""
-	https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/
+    """
+    https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/
 
 
-	"""
+    """
 
-	def __init__(self, username: str = Form(), password: str = Form()):
-		self.username = username
-		self.password = password
+    def __init__(self, username: str = Form(), password: str = Form()):
+        self.username = username
+        self.password = password
 
 
 class RegisterIn(BaseModel):
-	first_name: str
-	last_name: str
-	email: EmailStr
-	phone_number: str
-	password: str
-	phone_country_code: str = Field("1")
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone_number: str
+    password: str
+    phone_country_code: str = Field("1")
 
 
 class RegisterOut(BaseModel):
-	first_name: str
-	last_name: str
-	email: EmailStr
-	phone_number: str
-	phone_country_code: str = Field("1")
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone_number: str
+    phone_country_code: str
