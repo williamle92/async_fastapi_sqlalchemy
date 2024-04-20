4efa2e869a9c8f14fc8fe59f56521e33ecf9b75e
William Le <williamkle92@gmail.com>
2024-04-19 22:44:07 -0700

    feat: set redoc as main doc

--- backend/main.py
+++ backend/main.py
@@ -3,14 +3,14 @@ from backend.routers.users import router as user_router
 from backend.routers.auth import router as auth_router
 from backend.worker.tasks import add
 
-app: FastAPI = FastAPI()
+app: FastAPI = FastAPI(docs_url="/swagger", redoc_url="/")
 
 
 app.include_router(user_router)
 app.include_router(auth_router)
 
 
-@app.get("/")
+@app.get("/math")
 async def home():
     result = add.delay(4, 4)
     return {"math": str(result)}
